e14c6f29e3eb11c4f773fcbd8c299bd47f01ee01
JMDeS <jmichaeldeseno@gmail.com>
2018-08-09 03:37:30 -0400

    fixed parsing issue due to special char in password

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://jmdes:W34err%Tg@ds027425.mlab.com:27425/local_library';
+var mongoDB = 'mongodb://user:passw0rd@ds027425.mlab.com:27425/local_library';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
